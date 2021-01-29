be50f882bfab39e87feb2d80cc3228fa74283ab6
Aboubakary833 <aboubakarycisse472@gmail.com>
2021-01-29 13:31:59 +0100

    Ajout de la base de données

--- BD/simplon-appli (1).sql
+++ BD/simplon-appli (1).sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.2
+-- version 5.0.4
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le :  lun. 18 jan. 2021 à 12:38
--- Version du serveur :  10.4.11-MariaDB
--- Version de PHP :  7.3.13
+-- Généré le : ven. 29 jan. 2021 à 13:28
+-- Version du serveur :  10.4.17-MariaDB
+-- Version de PHP : 8.0.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -19,8 +18,27 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données :  `simplon-appli`
+-- Base de données : `simplon-appli`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `admin`
+--
+
+CREATE TABLE `admin` (
+  `username` varchar(100) NOT NULL,
+  `adminpassword` varchar(100) NOT NULL,
+  `directaccess` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `admin`
+--
+
+INSERT INTO `admin` (`username`, `adminpassword`, `directaccess`) VALUES
+('username', '1234', '1234');
 
 -- --------------------------------------------------------
 
@@ -30,14 +48,14 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `apprenants` (
   `ID` int(11) NOT NULL,
-  `Email` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `Email` varchar(255) NOT NULL,
   `Nom` varchar(50) NOT NULL,
   `Prenom` varchar(50) NOT NULL,
   `Dates_de_naissance` date NOT NULL,
   `Sexe` varchar(15) NOT NULL,
   `ville` varchar(255) NOT NULL,
   `formation` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `apprenants`
@@ -53,7 +71,8 @@ INSERT INTO `apprenants` (`ID`, `Email`, `Nom`, `Prenom`, `Dates_de_naissance`, 
 (15, 'alainguigma@gmail.com', 'Guigma', 'Alain', '1999-05-04', 'Masculin', 'Ouagadougou', 'Developpement web et web mobile'),
 (16, 'abdoulrachiddao@gmail.com', 'Dao', 'Abdoul Razack', '1998-01-01', 'Masculin', 'Bobo-Dioulasso', 'Référent digital'),
 (17, 'zionyirbabaresome@gmail.com', 'Somé', 'Romain', '1996-09-10', 'Masculin', 'Bobo-Dioulasso', 'Référent digital'),
-(18, 'kouamaelisabeth12@gmail.com', 'Koama', 'Elisabeth', '1996-06-05', 'Feminin', 'Ouagadougou', 'Référent digital');
+(18, 'kouamaelisabeth12@gmail.com', 'Koama', 'Elisabeth', '1996-06-05', 'Feminin', 'Ouagadougou', 'Référent digital'),
+(19, 'aboubakarycisse410@gmail.com', 'Cissé', 'Karim', '2004-05-11', 'Masculin', 'Ouagadougou', 'Développement web et web mobile');
 
 --
 -- Index pour les tables déchargées
@@ -73,7 +92,7 @@ ALTER TABLE `apprenants`
 -- AUTO_INCREMENT pour la table `apprenants`
 --
 ALTER TABLE `apprenants`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
